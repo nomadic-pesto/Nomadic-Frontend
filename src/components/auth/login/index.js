1f1e779ddd096d72338a20b9efddc64ad21186e2
Daniel Raj <68500777+raj-daniel@users.noreply.github.com>
2022-09-26 21:03:23 +0530

    added google login client

--- src/components/auth/login/index.js
+++ src/components/auth/login/index.js
@@ -8,6 +8,8 @@ import styles from "./styles.module.css";
 import * as Yup from "yup";
 import { Formik } from "formik";
 import { apiCall } from "../../../services/methods";
+import { GoogleLogin,GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
 
 const LogIn = () => {
     const validate = Yup.object({
@@ -18,25 +20,29 @@ const LogIn = () => {
             .min(8, "password must be atleast 8 character")
             .required("password is required"),
     });
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(event);
-    };
+    // const responseSuccessGoogle = (response) => {
+    //     console.log(response);
+    // };
+    // const responseErrorGoogle = (response) => {
+    //     console.log(response);
+    // };
     return (
-        <Formik
-            initialValues={{
-                email: "",
-                password: "",
-            }}
-            validationSchema={validate}
-        >
-            <div className={styles["login-background"]}>
-                <div className={styles["center-card"]}>
-                    <form
-                        className={styles["input-container"]}
-                        onSubmit={handleSubmit}
-                    >
+        <div className={styles["login-background"]}>
+            <div className={styles["center-card"]}>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                    }}
+                    validationSchema={validate}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    <form className={styles["input-container"]}>
                         <label>
                             Email Address:
                             <TextFieldComponent
@@ -60,23 +66,39 @@ const LogIn = () => {
                         <Button variant="contained" type="submit">
                             Log In
                         </Button>
-                        <Button className={styles["google-button"]}>
+                        {/* <Button className={styles["google-button"]}>
                             Google
-                        </Button>
+                        </Button> */}
                     </form>
-                    <div>
-                        <Stack spacing={12} direction="row">
-                            <ButtonTransparent>
-                                Forgot your password
-                            </ButtonTransparent>
-                            <ButtonTransparent variant="contained">
-                                Sign Up
-                            </ButtonTransparent>
-                        </Stack>
-                    </div>
+                </Formik>
+                <GoogleOAuthProvider clientId="817056518934-0p9ituunl6pnooif02pfgli1kr4n5ldh.apps.googleusercontent.com">
+                    <GoogleLogin
+                        onSuccess={(response) => {
+                            console.log(response);
+                            axios({
+                                method:"POST",
+                                url:"http://localhost:5001/v1/auth/googlelogin",
+                                data:{tokenId:response.credential}
+                            })
+                        }}
+                        onError={() => {
+                            console.log("Login Failed");
+                        }}
+                    />
+                    ;
+                </GoogleOAuthProvider>
+                <div>
+                    <Stack spacing={12} direction="row">
+                        <ButtonTransparent>
+                            Forgot your password
+                        </ButtonTransparent>
+                        <ButtonTransparent variant="contained">
+                            Sign Up
+                        </ButtonTransparent>
+                    </Stack>
                 </div>
             </div>
-        </Formik>
+        </div>
     );
 };
 
